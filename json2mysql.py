01d497a6689d3385d4bdc4531c1b71cc2e28fc01
Takashi SASAKI <takashi316@gmail.com>
2015-01-19 23:24:52 +0900

    varchar size and decimal digit lengths are configurable

--- json2mysql.py
+++ json2mysql.py
@@ -4,13 +4,20 @@ import MySQLdb
 import config
 import json
 import sys
+from decimal import Decimal
 
 
 def insert(conn, o):
   keys = ','.join(o.keys())
+
+  for k,v in o.items():
+    if isinstance(v, float):
+      o[k] = Decimal(v)
+
+  print(Decimal(o["time"]), type(o["time"]))
   s = "INSERT INTO %s.%s(%s) VALUES (%s)" % (config.DB, config.TABLE, keys, ','.join(["%s"]*len(o.values())))
-  print(s)
-  print(o.values())
+  #print(s)
+  #print(o.values())
   try:
     cursor = conn.cursor()
     cursor.execute(s, o.values())
@@ -27,11 +34,11 @@ def insert(conn, o):
         elif isinstance(v, long):
           columns.append(k + " " + "BIGINT")
         elif isinstance(v, str):
-          columns.append(k + " " + "VARCHAR(65535)") 
+          columns.append(k + " " + "VARCHAR(%s)" % config.VARCHAR_LENGTH) 
         elif isinstance(v, unicode):
-          columns.append(k + " " + "VARCHAR(65535)") 
-        elif isinstance(v, float):
-          columns.append(k + " " + "DOUBLE") 
+          columns.append(k + " " + "VARCHAR(%s)" % config.VARCHAR_LENGTH) 
+        elif isinstance(v, Decimal):
+          columns.append(k + " " + "DECIMAL(%s,%s)" % config.DECIMAL_DIGIT)
         else:
           raise Exception(type(v))
       print(columns)
@@ -82,9 +89,9 @@ def insert(conn, o):
           except MySQLdb.OperationalError as e2: 
             print(e2)
             pass
-        elif isinstance(v, float):
+        elif isinstance(v, Decimal):
           try: 
-            cursor.execute("ALTER TABLE %s ADD COLUMN %s DOUBLE" % (config.TABLE, k))
+            cursor.execute("ALTER TABLE %s ADD COLUMN %s DECIMAL(%s,%s)" % (config.TABLE, k, config.DECIMAL_DIGIT[0], config.DECIMAL_DIGIT[1]))
             conn.commit()
           except MySQLdb.OperationalError as e2: 
             print(e2)
