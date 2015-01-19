255d13423ec2a977c537dd32ee5138902e258a95
Takashi SASAKI <takashi316@gmail.com>
2015-01-19 18:40:50 +0900

    progress messages to stderr

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 DB="pcap"
-
+TABLE="pcap"
--- json2mysql.py
+++ json2mysql.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
+from __future__ import print_function
 import MySQLdb
 import config
+import json
+import sys
 
 if __name__ == "__main__":
     connector = MySQLdb.connect(db=config.DB)
@@ -8,6 +11,6 @@ if __name__ == "__main__":
     sql = "show databases"
     cursor.execute(sql)
     for row in cursor.fetchall():
-        print(row)
+        print(row, file=sys.stderr)
  
   
